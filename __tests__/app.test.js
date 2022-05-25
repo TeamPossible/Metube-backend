bb434cddba25168b6cfc57faceca2437ee82ff17
CarolynnFleming <cfcatpink@gmail.com>
2022-05-25 16:21:58 -0700

    register and login function, and set up mock user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,24 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+  
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
@@ -12,3 +30,7 @@ describe('alchemy-app routes', () => {
     pool.end();
   });
 });
+
+it('returns the current user', async () => {
+  const agent = request.agent(app);
+})
