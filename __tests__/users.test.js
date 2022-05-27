d66b00564edac98cd5a466b8a671aa961d0b8f56
Carolynn Fleming <cfcatpink@gmail.com>
2022-05-27 14:55:12 -0700

    Carrey (#10)
    * Dev (#1)
    
    * squl table set up beigining with users and profiles table
    
    * secrets added
    
    * set up cookie parser
    
    * users route created
    
    * jwt set up
    
    * signup user service
    
    * signin user service
    
    * register and login function, and set up mock user
    
    * test that returns the current user
    
    * test that returns the current user
    
    * it creates a user
    
    * .post
    
    * cunstructor made
    
    * insert a user
    
    * skip
    
    * return user controller
    
    * return a user by email
    
    * test to sign out user
    
    * successfully signs out a user
    
    * test that creates a description of the media
    
    * insert a media post
    
    * test thA\at creates a media post has passed
    
    * ?
    
    * added a await to userservice

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -49,7 +49,6 @@ describe('alchemy-app routes', () => {
       .post('/api/v1/users/sessions')
       .send({ email, password
       });
-
     expect(res.body).toEqual(expected);
     expect(res.status).toEqual(200);
   });
