574aea7c38c7ff62ed3a21e93b49759517911aa7
CadillacJack42 <72709991+CadillacJack42@users.noreply.github.com>
2022-05-27 11:14:00 -0700

    Jack (#8)
    * squl table set up beigining with users and profiles table
    
    * secrets added
    
    * set up cookie parser
    
    * users route created
    
    * jwt set up
    
    * signup user service
    
    * signin user service
    
    * register and login function, and set up mock user
    
    * test that returns the current user
    
    * test that returns the current user
    
    * it creates a user
    
    * .post
    
    * cunstructor made
    
    * insert a user
    
    * skip
    
    * Carrey (#3)
    
    * Dev (#1)
    
    * squl table set up beigining with users and profiles table
    
    * secrets added
    
    * set up cookie parser
    
    * users route created
    
    * jwt set up
    
    * signup user service
    
    * signin user service
    
    * register and login function, and set up mock user
    
    * test that returns the current user
    
    * test that returns the current user
    
    * it creates a user
    
    * .post
    
    * cunstructor made
    
    * insert a user
    
    * skip
    
    * return user controller
    
    * return a user by email
    
    * test to sign out user
    
    * successfully signs out a user
    
    * Kyras branch (#5)
    
    * Dev (#1)
    
    * squl table set up beigining with users and profiles table
    
    * secrets added
    
    * set up cookie parser
    
    * users route created
    
    * jwt set up
    
    * signup user service
    
    * signin user service
    
    * register and login function, and set up mock user
    
    * test that returns the current user
    
    * test that returns the current user
    
    * it creates a user
    
    * .post
    
    * cunstructor made
    
    * insert a user
    
    * skip
    
    * npm i'd
    
    * got rid of profiles table, moved keys to users table, tests pass
    
    Co-authored-by: Carolynn Fleming <cfcatpink@gmail.com>
    
    * Kyras branch (#7)
    
    * Dev (#1)
    
    * squl table set up beigining with users and profiles table
    
    * secrets added
    
    * set up cookie parser
    
    * users route created
    
    * jwt set up
    
    * signup user service
    
    * signin user service
    
    * register and login function, and set up mock user
    
    * test that returns the current user
    
    * test that returns the current user
    
    * it creates a user
    
    * .post
    
    * cunstructor made
    
    * insert a user
    
    * skip
    
    * npm i'd
    
    * got rid of profiles table, moved keys to users table, tests pass
    
    * added a route to app.js
    
    * added .cookie to .post
    
    * added cookie to post
    
    Co-authored-by: Carolynn Fleming <cfcatpink@gmail.com>
    
    * CHANGE: UserService method sighIn returns token and user object to be stored in local staorage by frontend
    
    Co-authored-by: CarolynnFleming <cfcatpink@gmail.com>
    Co-authored-by: Kyra Christensen <89879672+Kyra-christensen@users.noreply.github.com>

---
+++ __tests__/users.test.js
@@ -0,0 +1,73 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345',
+  username: 'bob'
+};
+
+describe('alchemy-app routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('creates a new user', async() => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const {  email, username } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+      username
+    });
+  });
+
+  it('returns the current user', async () => {
+    const agent = request.agent(app);
+
+    const user = await UserService.create({ ...mockUser, });
+
+    const { email } = user;
+    const password = mockUser.password;
+
+    const expected = {
+      message: 'Signed in successfully!',
+    };
+
+    const res = await agent
+      .post('/api/v1/users/sessions')
+      .send({ email, password
+      });
+
+    expect(res.body).toEqual(expected);
+    expect(res.status).toEqual(200);
+  });
+
+  it('should delete cookie from user object', async () => {
+    await UserService.create({ ...mockUser, });
+
+    const agent = request.agent(app);
+
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ ...mockUser });
+
+    const res = await agent.delete('/api/v1/users');
+
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Successfully signed out!',
+    });
+    expect(res.status).toEqual(200);
+
+  });
+
+});
